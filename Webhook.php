11536c62abea75a03d5ba43b1b507e5f2ab27b90
Sahil0198 <54398250+Sahil0198@users.noreply.github.com>
2019-09-02 18:56:47 +0530

    Update Webhook.php

--- Webhook.php
+++ Webhook.php
@@ -1,5 +1,5 @@
 <?php
-    $db_connection = pg_connect("host=Shopify-database dbname=d6e3ftk139ub4t user=ggmwcclozzctgy password=");
+    $db_connection = pg_connect("host=Shopify-database dbname=d6e3ftk139ub4t user=sahil password=sahil@0198");
     $result = pg_query($db_connection, "SELECT name FROM webhook.tbl_web");
     echo $result;
     echo "sahil";
