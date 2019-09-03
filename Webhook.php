c209c67935538cc98152ed27b7fa802fd71546da
Sahil0198 <54398250+Sahil0198@users.noreply.github.com>
2019-09-03 12:56:26 +0530

    Update Webhook.php

--- Webhook.php
+++ Webhook.php
@@ -9,7 +9,7 @@
 <h1>List of all names in the table</h1>
 <?php
 $db = pg_connect("host=ec2-174-129-227-146.compute-1.amazonaws.com port=5432 dbname=d6e3ftk139ub4t user=ggmwcclozzctgy password=2e72adfb860fa8fc865e14c969823ba27529638d9be4ea7bdf76dcca3bd97d5d");
-$result = pg_query($db,"SELECT * FROM tbl_web");
+$result = pg_query($db,"SELECT * FROM webhook.tbl_web");
 echo "<table>";
 while($row=pg_fetch_assoc($result)){echo "<tr>";
 echo "<td align='center' width='200'>" . $row['id'] . "</td>";
