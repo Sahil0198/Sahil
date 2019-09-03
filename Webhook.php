8800c9914ddfece2b0188085a7dffaf18dc0b08f
Sahil0198 <54398250+Sahil0198@users.noreply.github.com>
2019-09-03 11:47:19 +0530

    Update Webhook.php

--- Webhook.php
+++ Webhook.php
@@ -1,5 +1,6 @@
 <?php
     $db_connection = pg_connect("host=Shopify-database dbname=d6e3ftk139ub4t user=ggmwcclozzctgy password=");
-    $result = pg_query($db_connection, "SELECT name FROM tbl_web");
+    $qry="SELECT name FROM tbl_web WHERE id=1";
+    $result = pg_query($db_connection, $qry);
     echo $result;
 ?>
