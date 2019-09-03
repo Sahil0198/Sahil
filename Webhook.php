b098e16dbe72377997852a8154181879c831e98c
Sahil0198 <54398250+Sahil0198@users.noreply.github.com>
2019-09-03 12:32:33 +0530

    Update Webhook.php

--- Webhook.php
+++ Webhook.php
@@ -1,7 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<title>PostgreSQL SELECT Example 1</title>
+<meta name="description" content="If we want to fetch all rows from the actor table the following PostgreSQL SELECT statement can be used.">
+</head>
+<body>
+<h1>List of all names in the table</h1>
 <?php
-    $db_connection = pg_connect("ec2-174-129-227-146.compute-1.amazonaws.com","ggmwcclozzctgy","2e72adfb860fa8fc865e14c969823ba27529638d9be4ea7bdf76dcca3bd97d5d","d6e3ftk139ub4t");
-    $qry="SELECT name FROM tbl_web WHERE id=1";
-    $result = pg_query($db_connection, $qry);
-    echo $result;
-    echo "bbb";
-?>
+$db = pg_connect("host=ec2-174-129-227-146.compute-1.amazonaws.com port=5432 dbname=d6e3ftk139ub4t user=ggmwcclozzctgy password=2e72adfb860fa8fc865e14c969823ba27529638d9be4ea7bdf76dcca3bd97d5d");
+$result = pg_query($db,"SELECT * FROM tbl_web");
+echo "<table>";
+while($row=pg_fetch_assoc($result)){echo "<tr>";
+echo "<td align='center' width='200'>" . $row['id'] . "</td>";
+echo "<td align='center' width='200'>" . $row['name'] . "</td>";
+echo "</tr>";}echo "</table>";?>
+</div>
+</body>
+</html>
