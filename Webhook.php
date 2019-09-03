219c53b7f5175199d228abadbb22354be9c78835
Sahil0198 <54398250+Sahil0198@users.noreply.github.com>
2019-09-03 12:22:53 +0530

    Update Webhook.php

--- Webhook.php
+++ Webhook.php
@@ -1,6 +1,6 @@
 <?php
     $db_connection = pg_connect("ec2-174-129-227-146.compute-1.amazonaws.com","ggmwcclozzctgy","2e72adfb860fa8fc865e14c969823ba27529638d9be4ea7bdf76dcca3bd97d5d","d6e3ftk139ub4t");
-    $qry="SELECT name FROM webhook.tbl_web WHERE id=1";
+    $qry="SELECT name FROM tbl_web WHERE id=1";
     $result = pg_query($db_connection, $qry);
     echo $result;
     echo "bbb";
