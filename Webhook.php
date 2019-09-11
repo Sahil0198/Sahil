3846a69a02892b36224ddf58e23e768d5b29f564
Sahil0198 <54398250+Sahil0198@users.noreply.github.com>
2019-09-11 15:26:31 +0530

    Update Webhook.php

--- Webhook.php
+++ Webhook.php
@@ -1,11 +1,18 @@
 <?php
 	echo "sahil";
-	$webhook_content = '';
-	$webhook = fopen('php://input' , 'rb');
-	while(!feof($webhook)){ //loop through the input stream while the end of file is not reached
-		$webhook_content .= fread($webhook, 4096); //append the content on the current iteratio
-	fclose($webhook); //close the resource
-	$data = json_decode($webhook_content, true); //convert the json to array
-	echo $data[0];
-	}
+	header('Content-Type: application/json');
+class Shopify {
+	public function processOrderWebHook()
+		{
+			$webhook_content = '';
+			$webhook = fopen('php://input' , 'rb');
+			while(!feof($webhook)) //loop through the input stream while the end of file is not reached
+				{ 
+				$webhook_content .= fread($webhook, 4096); //append the content on the current iteratio
+				}
+			fclose($webhook); //close the resource
+			$data = json_decode($webhook_content, true); //convert the json to array
+			echo $data[0];
+		}
+}
 ?>
