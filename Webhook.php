e2c246b937ef4c93a23cc9e6d35c173edab22702
Sahil0198 <54398250+Sahil0198@users.noreply.github.com>
2019-09-11 17:41:49 +0530

    Update Webhook.php

--- Webhook.php
+++ Webhook.php
@@ -1,7 +1,10 @@
 <?php
+  $db = pg_connect("host=ec2-174-129-227-146.compute-1.amazonaws.com port=5432 dbname=d6e3ftk139ub4t user=ggmwcclozzctgy password=2e72adfb860fa8fc865e14c969823ba27529638d9be4ea7bdf76dcca3bd97d5d");
   $data = file_get_contents('php://input');
 if($data) {
   $data1 = json_decode($data, true);
+  $qry = "insert into webhook.webhook_tbl(product_data)values('$data1')";
+  $res=pg_query($db,$qry);
   $file = fopen("test.txt","w");
   fwrite($file,$data);
   fclose($file);
