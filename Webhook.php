8716b9155dc8e0f41bf8cbdb2dc2bb64cdb00cbc
Sahil0198 <54398250+Sahil0198@users.noreply.github.com>
2019-09-11 15:19:26 +0530

    Update Webhook.php

--- Webhook.php
+++ Webhook.php
@@ -3,9 +3,9 @@
 	$webhook_content = '';
 	$webhook = fopen('php://input' , 'rb');
 	while(!feof($webhook)){ //loop through the input stream while the end of file is not reached
-		$webhook_content .= fread($webhook, 4096); //append the content on the current iteration
-	}
+		$webhook_content .= fread($webhook, 4096); //append the content on the current iteratio
 	fclose($webhook); //close the resource
 	$data = json_decode($webhook_content, true); //convert the json to array
 	echo $data[0];
+	}
 ?>
