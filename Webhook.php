33cbee55d137d362099aba1c3ee2ff2c9090676d
Sahil0198 <54398250+Sahil0198@users.noreply.github.com>
2019-09-11 13:48:18 +0530

    Update Webhook.php

--- Webhook.php
+++ Webhook.php
@@ -1,3 +1,15 @@
 <?php
 	echo "sahil";
+	$webhook_content = '';
+	$webhook = fopen('php://input' , 'rb');
+	while(!feof($webhook)){ //loop through the input stream while the end of file is not reached
+		$webhook_content .= fread($webhook, 4096); //append the content on the current iteration
+	}
+	fclose($webhook); //close the resource
+	$data = json_decode($webhook_content, true); //convert the json to array
+	$headers = "From: KPStore <shop.support@kpstore.com> \r\n";
+	$headers .= "MIME-Version: 1.0\r\n";
+	$headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
+	echo mail("sahil.indybytes@gmail.com","Shopify Store: New Order Placed From Customer",$data,$headers);echo "\n";
+	echo file_put_contents("test.txt",$data);
 ?>
