7062ca60dc998f6f2ebce7cceb8eb343d30386e8
Sahil0198 <54398250+Sahil0198@users.noreply.github.com>
2019-09-11 17:19:39 +0530

    Update Webhook.php

--- Webhook.php
+++ Webhook.php
@@ -1,8 +1,9 @@
 <?php
   $data = file_get_contents('php://input');
 if($data) {
+  $data1 = json_decode($data, true);
   $file = fopen("test.txt","w");
-  fwrite($file,$data);
+  fwrite($file,$data1);
   fclose($file);
 }
 ?>
