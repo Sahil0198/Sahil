7d0cc4b5e9d183979474e8bd8c96d492f1cab2cf
Sahil0198 <54398250+Sahil0198@users.noreply.github.com>
2019-09-14 12:12:05 +0530

    Update Webhook.php

--- Webhook.php
+++ Webhook.php
@@ -4,7 +4,7 @@
 if($data) {
   $data1 = json_decode($data, true);
   $a = unserialize($data);
-  $qry = "insert into webhook.webhook_tbl(product_data)values('$a')";
+  $qry = "insert into webhook.webhook_tbl(id, product_data)values(1, '$a')";
   $res=pg_query($db,$qry);
   $file = fopen("test.txt","w");
   fwrite($file,print_r($data1,true));
