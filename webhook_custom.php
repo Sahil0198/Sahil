4894264fd86c672dc5fa1cbd6cad966a24edcc49
Sahil0198 <54398250+Sahil0198@users.noreply.github.com>
2019-09-17 11:46:49 +0530

    Update webhook_custom.php

--- webhook_custom.php
+++ webhook_custom.php
@@ -1,12 +1,13 @@
 <?php
-//   $db = pg_connect("host=ec2-174-129-227-146.compute-1.amazonaws.com port=5432 dbname=d6e3ftk139ub4t user=ggmwcclozzctgy password=2e72adfb860fa8fc865e14c969823ba27529638d9be4ea7bdf76dcca3bd97d5d");
-  $data = file_get_contents('php://input');
-if($data) {
-  $data1 = json_decode($data, true);
-//   $qry = "insert into webhook.product_data(json)values('$data')";
-//   $res=pg_query($db,$qry);
-  $file = fopen("test_custom.txt","w");
-  fwrite($file,print_r($data1,true));
-  fclose($file);
-}
+echo "sahil";
+// //   $db = pg_connect("host=ec2-174-129-227-146.compute-1.amazonaws.com port=5432 dbname=d6e3ftk139ub4t user=ggmwcclozzctgy password=2e72adfb860fa8fc865e14c969823ba27529638d9be4ea7bdf76dcca3bd97d5d");
+//   $data = file_get_contents('php://input');
+// if($data) {
+//   $data1 = json_decode($data, true);
+// //   $qry = "insert into webhook.product_data(json)values('$data')";
+// //   $res=pg_query($db,$qry);
+//   $file = fopen("test_custom.txt","w");
+//   fwrite($file,print_r($data1,true));
+//   fclose($file);
+// }
 ?>
