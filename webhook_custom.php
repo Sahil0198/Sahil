e6629e0e8a55a425add739120903105bbd092ce8
Sahil0198 <54398250+Sahil0198@users.noreply.github.com>
2019-09-19 12:03:54 +0530

    Update webhook_custom.php

--- webhook_custom.php
+++ webhook_custom.php
@@ -2,7 +2,7 @@
 <?php
 $url = 'https://087f5c7784a37deecd2bde33a2cb1e09:fb28a213fbcb9abba68d78aeff3e08d4@sahil-indybytes-98.myshopify.com';
    	$shopify = $url . '/admin/webhooks.json';
-	$arguments = array('topic' => 'products/update',
+	$arguments = array( 'webhook' => array('topic' => 'products/update',
 	'address' => 'https://shopifyhooks.herokuapp.com/Webhook.php',
 	'format' => 'json'
 	));
